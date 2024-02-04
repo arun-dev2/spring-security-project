a17ebb177be4f8e530e12a537f6e17cdd7d7b82b
arunp <arunp@DESKTOP-62D4RUP>
2024-02-04 21:25:58 +0530

    Added dafault user login and password encoder.

--- src/main/java/com/mvc/security/config/AppConfig.java
+++ src/main/java/com/mvc/security/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.mvc.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -20,5 +22,15 @@ public class AppConfig {
 		return vs;
 		
 	}
+	
+	
+	@Bean
+	PasswordEncoder getPasswordEncode(){
+		//return NoOpPasswordEncoder.getInstance();
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
 
 }
--- src/main/java/com/mvc/security/config/SecurityConfig.java
+++ src/main/java/com/mvc/security/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.mvc.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * This class is used to create spring security filter chains (fileter layers for request)
@@ -9,5 +14,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity //(debug=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	// create user details to byepass login page
+	
+	@Autowired
+	private PasswordEncoder bcryptPasswordEncoder;
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+		.withUser("arun")
+		.password("$2a$10$uFPnb/VrV.Eua7vBzP8OYeeUIYeiRd6xGTOb8xh6vOhcW1nuIeDtO")
+		.roles("Administrator");
+		
+		//System.out.println("password encode value : "+bcryptPasswordEncoder.encode("1234"));
+	}
+	
 }
